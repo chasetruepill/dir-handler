7b9e4b7fe956ef3ba7973a65f4fc0d443bcf78ff
Chase <chase@truepill.com>
2019-09-22 12:57:02 -0700

    simplify

--- src/dir-handler.ts
+++ src/dir-handler.ts
@@ -1,34 +1,30 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-function where (): string {
+function whereCalledFrom (): string {
   const _ = Error.prepareStackTrace
   Error.prepareStackTrace = (_, stack) => stack
-  const stack = new Error().stack!.slice(1)
+  const stack = new Error().stack!.slice(2)
   Error.prepareStackTrace = _
   const caller = (stack as any).find((c: NodeJS.CallSite) => c.getFileName() !== null)
 
   return path.dirname(caller.getFileName())
 }
 
-const dirHandler = (dir: string) => (handler: Function) => {
-  const files = fs.readdirSync(dir)
+export default (handler: Function) => {
+    const dir = whereCalledFrom()
+    const files = fs.readdirSync(dir)
 
-  const actionable = files.filter((filename: string) => {
-    const lowered = filename.toLowerCase()
+    const actionable = files.filter((filename: string) => {
+        const lowered = filename.toLowerCase()
 
-    return !['index.js', 'index.ts'].includes(lowered) && !lowered.startsWith('.') && ['.ts', '.js'].includes(lowered.slice(-3))
-  })
+        return !['index.js', 'index.ts'].includes(lowered) && !lowered.startsWith('.') && ['.ts', '.js'].includes(lowered.slice(-3))
+    })
 
-  return actionable.reduce((forExport: object, filename: string) => {
-    const content = require(path.join(dir, filename))
-    const e = handler({ filename: filename.substring(0, filename.length - 3), content: content.default || content })
+    return actionable.reduce((forExport: object, filename: string) => {
+        const content = require(path.join(dir, filename))
+        const e = handler({ filename: filename.substring(0, filename.length - 3), content: content.default || content })
 
-    return { ...forExport, ...e }
-  }, {})
-}
-
-export {
-  dirHandler,
-  where
+        return { ...forExport, ...e }
+    }, {})
 }
